b6c89711c912765af6da5247a1dc2a373ee72d8f
maksim <knyazev.maksim2852@gmail.com>
2024-03-13 16:24:09 +0400

    'new'

--- app/main.py
+++ app/main.py
@@ -1,118 +1,104 @@
 from contextlib import asynccontextmanager
 import time
-
-import sentry_sdk
+from app.prometheus.router import router as router_prometheus
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-from fastapi_versioning import VersionedFastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
 from redis import asyncio as aioredis
 from sqladmin import Admin
-
+from prometheus_fastapi_instrumentator import Instrumentator
 from app.admin.auth import authentication_backend
 from app.admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UsersAdmin
 from app.bookings.router import router as router_bookings
 from app.config.config import settings
 from app.database.database import engine
-
-# from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 from app.hotels.router import router as router_hotels
 from app.images.router import router as router_images
-from app.logger import logger
 from app.pages.router import router as router_pages
-from app.prometheus.router import router as router_prometheus
 from app.users.router_auth import router as router_auth
 from app.users.router_users import router as router_users
+import sentry_sdk
+from app.logger import logger
+from fastapi_versioning import VersionedFastAPI, version
 from app.importer.router import router as router_importer
 
-app = FastAPI(
-    title="Бронирование Отелей",
-    version="0.1.0",
-    root_path="/api",
-)
 
 
-# sentry_sdk.init(
-#     dsn=settings.SENTRY_DSN,
-#     traces_sample_rate=1.0,
-# )
-
-
-app.include_router(router_auth)
-app.include_router(router_users)
-# app.include_router(router_hotels)  # Необходимо реализовать самостоятельно
-app.include_router(router_bookings)
-app.include_router(router_rooms)
-app.include_router(router_hotels)
-app.include_router(router_images)
-app.include_router(router_prometheus)
-app.include_router(router_importer)
-
-origins = [
-    # 3000 - порт, на котором работает фронтенд на React.js 
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", 
-                   "Access-Control-Allow-Origin", "Authorization"],
-)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # при запуске
-    redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="cache")
+    redis = aioredis.from_url(f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}')
+    FastAPICache.init(RedisBackend(redis), prefix='cache')
+    print('Запущено')
     yield
-    # при выключении
+    print('Завершено')
 
 
-app = VersionedFastAPI(app,
-    version_format='{major}',
-    prefix_format='/api/v{major}',
-    lifespan=lifespan,
-)
 
+# создание приложения
+app = FastAPI(lifespan=lifespan, title='Островок', description='Бронирование отелей')
+
+
+
+
+# добавление роутера
+app.include_router(router_users)
+app.include_router(router_auth)
+app.include_router(router_bookings)
+app.include_router(router_rooms)
+app.include_router(router_hotels)
 app.include_router(router_pages)
-
-if settings.MODE == "TEST":
-    redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="cache")
-
+app.include_router(router_images)
+app.include_router(router_importer)
+app.include_router(router_prometheus)
 
 
 instrumentator = Instrumentator(
     should_group_status_codes=False,
-    excluded_handlers=[".*admin.*", "/metrics"],
+    excluded_handlers=['.*admin', '/metrics']
 )
 instrumentator.instrument(app).expose(app)
 
+app = VersionedFastAPI(app, 
+    version_format='{major}',
+    prefix_format='/v{major}'
+)
 
-admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+
+
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
+admin = Admin(app, engine=engine, authentication_backend=authentication_backend)
 admin.add_view(UsersAdmin)
 admin.add_view(HotelsAdmin)
-admin.add_view(RoomsAdmin)
 admin.add_view(BookingsAdmin)
+admin.add_view(RoomsAdmin)
 
-app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    # При подключении Prometheus + Grafana подобный лог не требуется
-    logger.info("Request handling time", extra={
-        "process_time": round(process_time, 4)
-    })
-    return response
+
+
+
+sentry_sdk.init(
+    dsn='https://141592df8d6868f69bf447a5d622438c@o4506869734309888.ingest.us.sentry.io/4506869736538112',
+    traces_sample_rate=1.0
+)
+
+
+
+
+
+
+# @app.middleware('http')
+# async def add_proccess_time_handler(request: Request, call_next):
+#     start_time = time.time()
+#     response = await call_next(request)
+#     proccess_time = time.time() - start_time
+#     response.headers['X-Proccess-Time'] = str(proccess_time)
+#     logger.info('Request execution time', extra={
+#         'proccess_time': round(proccess_time, 4)
+#     })
+#     return response
